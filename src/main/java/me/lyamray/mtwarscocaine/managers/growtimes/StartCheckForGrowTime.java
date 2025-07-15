8df82153de50289595a4178098e7503163d8bb50
LyamRay <lyam.ray410@gmail.com>
2025-07-15 21:19:33 +0200

    Updated.

--- src/main/java/me/lyamray/mtwarscocaine/managers/growtimes/StartCheckForGrowTime.java
+++ src/main/java/me/lyamray/mtwarscocaine/managers/growtimes/StartCheckForGrowTime.java
@@ -3,9 +3,12 @@ package me.lyamray.mtwarscocaine.managers.growtimes;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 import me.lyamray.mtwarscocaine.MTWarsCocaine;
+import me.lyamray.mtwarscocaine.database.LoadPlants;
 import org.bukkit.Bukkit;
 
+import java.sql.SQLException;
 import java.time.LocalTime;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -14,17 +17,24 @@ public class StartCheckForGrowTime {
     @Getter
     private static final StartCheckForGrowTime instance = new StartCheckForGrowTime();
 
+    @SneakyThrows
     public void startTimeCheckTask() {
         Bukkit.getScheduler().runTaskTimer(MTWarsCocaine.getInstance(), this::checkTimeAndLog, 0L, 1200L);
     }
 
-    private void checkTimeAndLog() {
+    private void checkTimeAndLog() throws SQLException {
         MTWarsCocaine.setPlantsCanGrow(RandomGrowTime.getInstance().isWithinAnyRange(LocalTime.now()));
         if (RandomGrowTime.getInstance().isWithinAnyRange(LocalTime.now())) {
-            MTWarsCocaine.getInstance().load
+            try {
+                LoadPlants.getInstance().load();
+            } catch (SQLException e) {
+               throw new SQLException("Could not load the plants from the database." + e);
+            }
         }
+
         String message = RandomGrowTime.getInstance().isWithinAnyRange(LocalTime.now())
-                ? "Lyam zijn ballen zijn " + Math.PI + " cm in diameter!" : "Je bent maagd";
+                ? "Lyam zijn ballen zijn " + Math.PI + " cm in diameter!"
+                : "Je bent maagd";
         Bukkit.broadcastMessage(message);
     }
 }
